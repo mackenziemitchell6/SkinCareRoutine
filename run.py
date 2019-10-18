6e9db20fd6d4806c1135ff46a38495c2f498e92c
Mackenzie Mitchell <mackenziemitchell6@gmail.com>
2019-10-18 16:13:53 -0400

    all for today

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 11:51:53 2019
+
+@author: mackenziemitchell
+"""
+
+from dash import app
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
